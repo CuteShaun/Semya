4c6c5ad0be4bdd87a6e8bc4c751abe7e25e17211
CuteShaun <andrewtsapko@gmail.com>
2019-02-15 03:13:51 +0200

    Init project

---
+++ js/script.js
@@ -0,0 +1,70 @@
+window.addEventListener("resize", function() {
+  changeControlHeight();
+});
+
+function changeControlHeight() {
+  const carouselControl = document.getElementById("carousel-control");
+  let carouselImg = document.querySelector(
+    ".carousel-item.active .carousel__img"
+  );
+  carouselControl.style.height = `${carouselImg.clientHeight}px`;
+}
+
+function multipleCarousel() {
+  $("#recipeCarousel").carousel({
+    interval: 10000
+  });
+
+  $(".carousel-1 .carousel-item").each(function() {
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(":first");
+    }
+    next
+      .children(":first-child")
+      .clone()
+      .appendTo($(this));
+
+    for (var i = 0; i < 2; i++) {
+      next = next.next();
+      if (!next.length) {
+        next = $(this).siblings(":first");
+      }
+
+      next
+        .children(":first-child")
+        .clone()
+        .appendTo($(this));
+    }
+  });
+
+  $("#recipeCarousel-1").carousel({
+    interval: 10000
+  });
+
+  $(".carousel-2 .carousel-item").each(function() {
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(":first");
+    }
+    next
+      .children(":first-child")
+      .clone()
+      .appendTo($(this));
+
+    for (var i = 0; i < 2; i++) {
+      next = next.next();
+      if (!next.length) {
+        next = $(this).siblings(":first");
+      }
+
+      next
+        .children(":first-child")
+        .clone()
+        .appendTo($(this));
+    }
+  });
+}
+
+multipleCarousel();
+changeControlHeight();
