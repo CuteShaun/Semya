14333e13245253a5cbf8a547eec9c3b7296c2897
CuteShaun <andrewtsapko@gmail.com>
2019-02-23 23:33:43 +0200

    Responsive header and footer

--- js/script.js
+++ js/script.js
@@ -71,3 +71,21 @@ changeControlHeight();
 
 
 //hamburger
+const hamburger = document.getElementById("hamburger");
+const hamburgerClose = document.getElementById("hamburger-close")
+const mobileNav = document.getElementById("mobile-menu");
+const overlay = document.getElementById("overlay");
+
+hamburger.addEventListener("click", openMenu);
+hamburgerClose.addEventListener("click", closeMenu);
+
+function openMenu() {
+  mobileNav.classList.add("hamburger-menu--active");
+  console.log(overlay, 'gg');
+  overlay.classList.add("overlay--active")
+}
+
+function closeMenu() {
+  mobileNav.classList.remove("hamburger-menu--active");
+  overlay.classList.remove("overlay--active")
+}
