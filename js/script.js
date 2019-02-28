20181690eecac255f88f8c90f8014c1c8e94902c
CuteShaun <andrewtsapko@gmail.com>
2019-02-28 21:10:57 +0200

    Slick slider and adaptive form

--- js/script.js
+++ js/script.js
@@ -1,74 +1,85 @@
-window.addEventListener("resize", function() {
-  changeControlHeight();
+// window.addEventListener("resize", function() {
+//   changeControlHeight();
+// });
+
+// function changeControlHeight() {
+//   const carouselControl = document.getElementById("carousel-control");
+//   let carouselImg = document.querySelector(
+//     ".carousel-item.active .carousel__img"
+//   );
+//   carouselControl.style.height = `${carouselImg.clientHeight}px`;
+// }
+
+// function multipleCarousel() {
+//   $("#recipeCarousel").carousel({
+//     interval: 10000
+//   });
+
+//   $(".carousel-1 .carousel-item").each(function() {
+//     var next = $(this).next();
+//     if (!next.length) {
+//       next = $(this).siblings(":first");
+//     }
+//     next
+//       .children(":first-child")
+//       .clone()
+//       .appendTo($(this));
+
+//     for (var i = 0; i < 2; i++) {
+//       next = next.next();
+//       if (!next.length) {
+//         next = $(this).siblings(":first");
+//       }
+
+//       next
+//         .children(":first-child")
+//         .clone()
+//         .appendTo($(this));
+//     }
+//   });
+
+//   $("#recipeCarousel-1").carousel({
+//     interval: 10000
+//   });
+
+//   $(".carousel-2 .carousel-item").each(function() {
+//     var next = $(this).next();
+//     if (!next.length) {
+//       next = $(this).siblings(":first");
+//     }
+//     next
+//       .children(":first-child")
+//       .clone()
+//       .appendTo($(this));
+
+//     for (var i = 0; i < 2; i++) {
+//       next = next.next();
+//       if (!next.length) {
+//         next = $(this).siblings(":first");
+//       }
+
+//       next
+//         .children(":first-child")
+//         .clone()
+//         .appendTo($(this));
+//     }
+//   });
+// }
+
+// multipleCarousel();
+// changeControlHeight();
+function newSlickInstance(params) {
+  let {prev, next} = params;
+  $('#main-slider').slick({
+    prevArrow: $(prev),
+    nextArrow: $(next)
+  });
+}
+
+newSlickInstance({
+  prev: ".main-slider__prev",
+  next: ".main-slider__next"
 });
-
-function changeControlHeight() {
-  const carouselControl = document.getElementById("carousel-control");
-  let carouselImg = document.querySelector(
-    ".carousel-item.active .carousel__img"
-  );
-  carouselControl.style.height = `${carouselImg.clientHeight}px`;
-}
-
-function multipleCarousel() {
-  $("#recipeCarousel").carousel({
-    interval: 10000
-  });
-
-  $(".carousel-1 .carousel-item").each(function() {
-    var next = $(this).next();
-    if (!next.length) {
-      next = $(this).siblings(":first");
-    }
-    next
-      .children(":first-child")
-      .clone()
-      .appendTo($(this));
-
-    for (var i = 0; i < 2; i++) {
-      next = next.next();
-      if (!next.length) {
-        next = $(this).siblings(":first");
-      }
-
-      next
-        .children(":first-child")
-        .clone()
-        .appendTo($(this));
-    }
-  });
-
-  $("#recipeCarousel-1").carousel({
-    interval: 10000
-  });
-
-  $(".carousel-2 .carousel-item").each(function() {
-    var next = $(this).next();
-    if (!next.length) {
-      next = $(this).siblings(":first");
-    }
-    next
-      .children(":first-child")
-      .clone()
-      .appendTo($(this));
-
-    for (var i = 0; i < 2; i++) {
-      next = next.next();
-      if (!next.length) {
-        next = $(this).siblings(":first");
-      }
-
-      next
-        .children(":first-child")
-        .clone()
-        .appendTo($(this));
-    }
-  });
-}
-
-multipleCarousel();
-changeControlHeight();
-
 
 //hamburger
 const hamburger = document.getElementById("hamburger");
