c6b7fb5701521d270af7a569206a4f1991707a79
CuteShaun <andrewtsapko@gmail.com>
2019-03-18 17:07:54 +0200

    Checkout-page

--- js/hamburger.js
+++ js/hamburger.js
@@ -17,3 +17,18 @@ function closeMenu() {
   mobileNav.classList.remove("hamburger-menu--active");
   overlay.classList.remove("overlay--active")
 }
+
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    // При создании объекта карты необходимо указать его свойства
+    // center - определяем точку на которой карта будет центрироваться
+    center: { lat: 46.471, lng: 30.744 },
+    // zoom - определяет масштаб. 0 - видно всю платнеу. 18 - видно дома и улицы города.
+    zoom: 17
+  });
+}
+
+
+// 46.4712047,30.7449723,17z
--- js/script.js
+++ js/script.js
@@ -1,102 +1,136 @@
-// window.addEventListener("resize", function() {
-//   changeControlHeight();
-// });
+window.addEventListener(
+  "resize",
+  function() {
+    changeControlHeight();
+  },
+  { passive: true }
+);
 
-// function changeControlHeight() {
-//   const carouselControl = document.getElementById("carousel-control");
-//   let carouselImg = document.querySelector(
-//     ".carousel-item.active .carousel__img"
-//   );
-//   carouselControl.style.height = `${carouselImg.clientHeight}px`;
-// }
+function changeControlHeight() {
+  const carouselControl1 = document.getElementById(
+    "multiple-one-carousel__control"
+  );
 
-// function multipleCarousel() {
-//   $("#recipeCarousel").carousel({
-//     interval: 10000
-//   });
+  const carouselControl2 = document.getElementById(
+    "multiple-one-carousel__control-2"
+  );
 
-//   $(".carousel-1 .carousel-item").each(function() {
-//     var next = $(this).next();
-//     if (!next.length) {
-//       next = $(this).siblings(":first");
-//     }
-//     next
-//       .children(":first-child")
-//       .clone()
-//       .appendTo($(this));
+  let carouselImg = document.querySelector(
+    ".slick-current .multiple-one-carousel__img"
+  );
+  let carouselImg2 = document.querySelector(
+    ".slick-current .multiple-one-carousel__img--second"
+  );
+  carouselControl1.style.height = `${carouselImg.clientHeight}px`;
+  carouselControl2.style.height = `${carouselImg2.clientHeight}px`;
+}
 
-//     for (var i = 0; i < 2; i++) {
-//       next = next.next();
-//       if (!next.length) {
-//         next = $(this).siblings(":first");
-//       }
-
-//       next
-//         .children(":first-child")
-//         .clone()
-//         .appendTo($(this));
-//     }
-//   });
-
-//   $("#recipeCarousel-1").carousel({
-//     interval: 10000
-//   });
-
-//   $(".carousel-2 .carousel-item").each(function() {
-//     var next = $(this).next();
-//     if (!next.length) {
-//       next = $(this).siblings(":first");
-//     }
-//     next
-//       .children(":first-child")
-//       .clone()
-//       .appendTo($(this));
-
-//     for (var i = 0; i < 2; i++) {
-//       next = next.next();
-//       if (!next.length) {
-//         next = $(this).siblings(":first");
-//       }
-
-//       next
-//         .children(":first-child")
-//         .clone()
-//         .appendTo($(this));
-//     }
-//   });
-// }
-
-// multipleCarousel();
-// changeControlHeight();
 function newSlickInstance(params) {
-  let {prev, next} = params;
-  $('#main-slider').slick({
+  let {
+    prev,
+    next,
+    selector,
+    slideShow,
+    slideScroll,
+    infinite,
+    responsive,
+    classNameCSS
+  } = params;
+
+  $(selector).slick({
     prevArrow: $(prev),
-    nextArrow: $(next)
+    nextArrow: $(next),
+    infinite: infinite,
+    slidesToShow: slideShow,
+    slidesToScroll: slideScroll,
+    responsive: responsive
   });
 }
 
 newSlickInstance({
+  selector: "#main-slider",
   prev: ".main-slider__prev",
   next: ".main-slider__next"
 });
 
-//hamburger
+newSlickInstance({
+  selector: "#multiple-one-carousel",
+  infinite: true,
+  slideShow: 3,
+  slideScroll: 1,
+  next: ".multiple-one-carousel__control",
+  classNameCSS: ".multiple-one-carousel",
+  responsive: [
+    {
+      breakpoint: 980, //tablet breakpoint
+      settings: {
+        slidesToShow: 2
+      }
+    }
+  ]
+});
+
+newSlickInstance({
+  selector: "#multiple-one-carousel--second",
+  infinite: true,
+  slideShow: 3,
+  slideScroll: 1,
+  next: ".multiple-one-carousel__control--second",
+  classNameCSS: ".multiple-one-carousel",
+  responsive: [
+    {
+      breakpoint: 980, //tablet breakpoint
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+newSlickInstance({
+  selector: "#country-slider",
+  slideShow: 4,
+  slideScroll: 4,
+  prev: ".country-slider__prev",
+  next: ".country-slider__next",
+  classNameCSS: ".country-slider",
+  responsive: [
+    {
+      breakpoint: 768, //tablet breakpoint
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+  ]
+});
+
+//reset slider position in tabs
+$('a[data-toggle="tab"]').on("shown.bs.tab", { passive: true }, function(e) {
+  e.target;
+  e.relatedTarget;
+  $(".multiple-one-carousel").slick("setPosition");
+  changeControlHeight();
+});
+
+// hamburger;
 const hamburger = document.getElementById("hamburger");
-const hamburgerClose = document.getElementById("hamburger-close")
+const hamburgerClose = document.getElementById("hamburger-close");
 const mobileNav = document.getElementById("mobile-menu");
 const overlay = document.getElementById("overlay");
 
-hamburger.addEventListener("click", openMenu);
-hamburgerClose.addEventListener("click", closeMenu);
+hamburger.addEventListener("click", openMenu, { passive: true });
+hamburgerClose.addEventListener("click", closeMenu, { passive: true });
 
 function openMenu() {
   mobileNav.classList.add("hamburger-menu--active");
-  console.log(overlay, 'gg');
-  overlay.classList.add("overlay--active")
+  console.log(overlay, "gg");
+  overlay.classList.add("overlay--active");
 }
 
 function closeMenu() {
   mobileNav.classList.remove("hamburger-menu--active");
-  overlay.classList.remove("overlay--active")
+  overlay.classList.remove("overlay--active");
 }
+
+changeControlHeight();
